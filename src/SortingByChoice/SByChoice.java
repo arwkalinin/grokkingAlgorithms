810675af6b53bf7c4121ece5dbcd4e26ea523b12
Alexander Kalinin <arw.kalinin@gmail.com>
2022-05-01 13:27:40 +0300

    sorting by choice method

--- src/SortingByChoice/SByChoice.java
+++ src/SortingByChoice/SByChoice.java
@@ -10,7 +10,7 @@ public class SByChoice {
     public static void main(String[] args) {
 
         ArrayList<MusicBand> bandsList = new ArrayList<>();
-        bandsList.add(new MusicBand("RAMMSTEIN", 350));
+        bandsList.add(new MusicBand("RAMMSTEIN", 650));
         bandsList.add(new MusicBand("LINKIN PARK", 425));
         bandsList.add(new MusicBand("RADIOHEAD", 50));
         bandsList.add(new MusicBand("KISHORE KUMAR", 13));
@@ -19,9 +19,35 @@ public class SByChoice {
         bandsList.add(new MusicBand("NEUTRAL MILK HOTEL", 17));
         bandsList.add(new MusicBand("THE BLACK KEYS", 39));
 
-        System.out.println(bandsList);
+        System.out.println("INPUT LIST: " + bandsList);
 
-        MusicBand[] bandsListArray = bandsList.toArray(new MusicBand[bandsList.size()]);
-        MusicBand selectedBand = null;
+        bandsList = getSortedToHigherArrayList(bandsList);
+
+        System.out.println("SORTED LIST: " + bandsList);
     }
+
+    private static int getIndexOfSmallestAuditionCount(ArrayList<MusicBand> inputList) {
+        MusicBand smallestBandByAudition = inputList.get(0);
+        int smallestIndex = 0;
+        for (int i = 1; i < inputList.size(); i++) {
+            if (inputList.get(i).getAuditionCount() < inputList.get(smallestIndex).getAuditionCount()) {
+                smallestBandByAudition = inputList.get(i);
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    private static ArrayList<MusicBand> getSortedToHigherArrayList(ArrayList<MusicBand> inputList) {
+        ArrayList<MusicBand> inputListCopy = new ArrayList<>(inputList);
+        ArrayList<MusicBand> newList = new ArrayList<>();
+        int curSmallIndex;
+        for (int i = 0; i < inputList.size(); i++) {
+            curSmallIndex = getIndexOfSmallestAuditionCount(inputListCopy);
+            newList.add(inputListCopy.get(curSmallIndex));
+            inputListCopy.remove(curSmallIndex);
+        }
+        return newList;
+    }
+
 }
